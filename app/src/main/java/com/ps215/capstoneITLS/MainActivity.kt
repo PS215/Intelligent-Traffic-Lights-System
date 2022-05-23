5f4bb60ad41f3b4044208d47726b411b1fa79c21
Amjadaziz10 <muhammadamjadaziz2012@gmail.com>
2022-05-24 02:42:03 +0700

    initial commit

---
+++ app/src/main/java/com/ps215/capstoneITLS/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ps215.capstoneITLS
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
